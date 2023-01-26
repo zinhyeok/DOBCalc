0d628b29bf290ce2510c4052c8646e736cf74aff
Jinhyeok Park <86557453+zinhyeok@users.noreply.github.com>
2023-01-26 15:30:54 +0900

    [Feat]: datepicker add 87

--- app/src/main/java/zin/com/dobcalc/MainActivity.kt
+++ app/src/main/java/zin/com/dobcalc/MainActivity.kt
@@ -1,11 +1,39 @@
 package zin.com.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.Toast
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById<Button>(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener(){
+            clickDatePicker()
+        }
     }
+
+    fun clickDatePicker(){
+        val myCalender = Calendar.getInstance()
+        val year = myCalender.get(Calendar.YEAR)
+        val month = myCalender.get(Calendar.MONTH)
+        val day = myCalender.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this,
+        DatePickerDialog.OnDateSetListener{view, year, month, dayOfMonth ->
+            Toast.makeText(this,
+                "pressed run", Toast.LENGTH_LONG).show()
+        },
+        year, month, day
+            ).show()
+
+    }
+
+
 }
